bfd5c45c22e845ef24f64fdb197a6629d69ebd94
Fabien ZARIFIAN <fabien.zarifian@nuevolia.fr>
2019-09-16 13:26:16 +0200

    Updates

---
+++ styles-2473e95ac2f770392436.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{177:function(r,b,g){r.exports={lighter:"rgb(55, 57, 64)",dark:"rgb(40, 44, 52)",darker:"rgb(32, 35, 42)",brand:"rgb(97, 218, 251)",brandLight:"rgb(187, 239, 253)",text:"rgb(26, 26, 26)",subtle:"rgb(109, 109, 109)",subtleOnDark:"rgb(153, 153, 153)",divider:"rgb(236, 236, 236)",note:"rgb(255, 229, 100)",error:"rgb(255, 100, 100)",white:"rgb(255, 255, 255)",black:"rgb(0, 0, 0)"}},204:function(r,b,g){},205:function(r,b,g){}}]);
+//# sourceMappingURL=styles-2473e95ac2f770392436.js.map
